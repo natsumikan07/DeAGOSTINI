42f81aa19985049b851baabf50f1b7b2655b09fb
natsumikan07 <m.natsumi07@gmail.com>
2021-01-16 15:01:25 +0000

    if文の問題、解答作成

---
+++ secIf/IfAnswer.java
@@ -0,0 +1,13 @@
+class IfAnswer {
+  public static void main(String[] args) {
+    int number = 1;
+    
+    if (number < 1) {
+      System.out.println("ちがうよ");
+
+    } else {
+
+      System.out.println("そうだよ");
+    }
+  }
+}
---
+++ secIf/IfQuestion.java
@@ -0,0 +1,9 @@
+class IfQuestion {
+  public static void main(String[] args) {
+    int number = 1;
+
+      System.out.println("ちがうよ");
+
+      System.out.println("そうだよ");
+  }
+}
